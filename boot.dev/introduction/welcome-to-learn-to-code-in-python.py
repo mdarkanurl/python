19c8fde51d56b8ef180fcd577e5d1c84df9e76e3
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-25 12:37:44 +0600

    Added welcome to learn to code in python lesson

---
+++ boot.dev/introduction/welcome-to-learn-to-code-in-python.py
@@ -0,0 +1,10 @@
+# In JavaScript we use console.log("Welcome to my world!") to print something in console
+# but in Python we use this 👇.
+
+print("Welcome to my world!")
+print("Learning Python is fun!")
+
+# Run => python .\boot.dev\introduction\welcome-to-learn-to-code-in-python.py from root dir to see the output.
+# Output would be 👇:
+    # Welcome to my world!
+    # Learning Python is fun!
